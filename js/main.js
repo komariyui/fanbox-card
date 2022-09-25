d6863687b27f7ce52d1bc9addccb4ef7c55d1da6
shiratama <lightsnow4958@gmail.com>
2022-09-26 04:17:42 +0800

    feat:光膜完全完成

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ window.onload = function (){
         let x = (event.clientX-coreX)/coreX;
         let y = (event.clientY-coreY)/coreY;
         restDraw(x,y);
+        restDraw2(x,y);
         document.getElementById("main-shadow-image").style.transform = `perspective(${x+1600}px) rotateX(${y*21}deg) rotateY(${(x*18)>0?-(x*18):Math.abs(x*18)}deg) `
 
     })
@@ -34,7 +35,7 @@ window.onload = function (){
         await mainImg.drawImage(newMainImg,0,0,1280,800);
     }
 
-
+/*----------图Y*/
     let img = new Image()
     let img2 = new Image()
     imgDraw();
@@ -61,18 +62,9 @@ window.onload = function (){
         const tempCanvas = document.createElement('canvas'); // 缓存 canvas
         const tempCtx = tempCanvas.getContext('2d');
         tempCanvas.width = 1280; tempCanvas.height = 800; // 设置宽高
-        // render();
-        // console.log("被调用")
-        // console.log(cnv.height);
-        // cnw.width = cnw.width;
-        // cnw.fillStyle = "#FFF"
-        // imgDraw()
-        // console.log(img)
-        // cnw.drawImage(img,0, 0,3280,1800);
-        // cnw.globalCompositeOperation = "destination-in";
-        // cnw.drawImage(img2,0, 0,1280,800);
-        // console.log(img2)
+
         cnw.globalCompositeOperation = "source-over"
+
         img.src = "../images/color-y.png" //颜色
         img.onload = async function () {
             // console.log("颜色加载完成")
@@ -89,6 +81,55 @@ window.onload = function (){
         }
 
     };
+
+    /*----------图X*/
+    let img3 = new Image()
+    let img4 = new Image()
+    const cnv1 = document.getElementById('mainShadowBackground-1');
+    const cnw2 = cnv1.getContext('2d')
+    imgDraw2();
+
+    function imgDraw2(){
+        img3.src = "../images/color-y.png" //颜色
+        img3.onload = async function () {
+            // console.log("颜色加载完成")
+            await cnw2.drawImage(img,0, 0,6280,3800);
+            cnw2.globalCompositeOperation = "destination-in";  //保留source-in
+            img4.src = "../images/mosaic-1.png" //格子
+            img4.onload = async function (){
+                // console.log("格子加载完成")
+                await cnw2.drawImage(img2,0, 0,1280,800);
+            }
+        }
+    }
+
+
+
+    function restDraw2(x,y){
+        const tempCanvas = document.createElement('canvas'); // 缓存 canvas
+        const tempCtx = tempCanvas.getContext('2d');
+        tempCanvas.width = 1280; tempCanvas.height = 800; // 设置宽高
+
+        cnw2.globalCompositeOperation = "source-over"
+
+        img.src = "../images/color-x.png" //颜色
+        img.onload = async function () {
+            // console.log("颜色加载完成")
+            console.log(x+"}}"+y)
+            await tempCtx.drawImage(img,-500+x*600, -500+y*600,6280,3800);
+            tempCtx.globalCompositeOperation = "destination-in";
+            img2.src = "../images/mosaic-2.png" //格子
+            img2.onload =function (){
+                // console.log("格子加载完成")
+                tempCtx.drawImage(img2,0, 0,1280,800);
+                cnw2.clearRect(0,0,cnv.width,cnv.height);
+                cnw2.drawImage(tempCanvas,0,0);
+            }
+
+        }
+
+    };
+
     //无双缓存
     // function restDraw(x,y){
     //    // render();
